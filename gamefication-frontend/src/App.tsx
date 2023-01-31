3245eea1976b4057de18a6e45915ce6a48cef9a6
MrAziAti <aziz.atif87@gmail.com>
2023-01-31 11:55:24 +0100

    components and UI folders created

--- gamefication-frontend/src/App.tsx
+++ gamefication-frontend/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from "@monaco-editor/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="relative">
+            <Editor className="absolute right-0 text-green-800"
+                height="100vh"
+                width="100VH"
+                defaultLanguage="javascript"
+                defaultValue="// some comment"
+            />
+
+        </div>
+    )
+        ;
 }
 
 export default App;
