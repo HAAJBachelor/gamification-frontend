c2cd5ce3d549d0eddc01bb39f7967ca43f2bf838
MrAziAti <aziz.atif87@gmail.com>
2023-02-01 11:22:44 +0100

    Added a route to rulespage

--- gamefication-frontend/src/App.tsx
+++ gamefication-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route, Router} from "react-router-dom";
 import GameEditor from "./components/CodeEditor/GameEditor";
 import MainPaige from "./components/Pages/MainPaige";
+import RulesPage from "./components/Pages/RulesPage";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainPaige />}>
                     <Route path="/editor" element={<GameEditor/>} />
+                    <Route path="/rules" element={<RulesPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
