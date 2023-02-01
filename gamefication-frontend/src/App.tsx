7bfd439fc210fc26aa94a525d5a060d87e838350
MrAziAti <aziz.atif87@gmail.com>
2023-02-01 12:43:17 +0100

    Routes works now as they should

--- gamefication-frontend/src/App.tsx
+++ gamefication-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route, Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import GameEditor from "./components/CodeEditor/GameEditor";
 import MainPaige from "./components/Pages/MainPaige";
 import RulesPage from "./components/Pages/RulesPage";
@@ -8,14 +8,14 @@ import RulesPage from "./components/Pages/RulesPage";
 function App() {
 
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
-                <Route path="/" element={<MainPaige />}>
-                    <Route path="/editor" element={<GameEditor/>} />
-                    <Route path="/rules" element={<RulesPage/>}/>
-                </Route>
+                <Route path="/" element={<MainPaige/>}/>
+                <Route path="/editor" element={<GameEditor/>}/>
+                <Route path="/rules" element={<RulesPage/>}/>
+
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
 
--- gamefication-frontend/src/components/Pages/MainPaige.tsx
+++ gamefication-frontend/src/components/Pages/MainPaige.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Outlet, Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import Card from "../UI/Card";
 
+
 const MainPaige = () => {
+
     return (
         <Card className="flex items-center justify-center h-screen bg-black">
             <div className="border-4 border-yellow-500 p-48">
                 <h1 className='text-white'>Hello world</h1>
+
+                <button className='text-white border-1 border-white-500'>Helloooo<Link to='/editor'></Link></button>
+                <Outlet/>
             </div>
 
         </Card>
