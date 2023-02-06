8643b7a7011f071775dcd3941d2b7846b9d66f0f
MrAziAti <aziz.atif87@gmail.com>
2023-02-06 09:01:42 +0100

    Added onClick to Button component

--- gamefication-frontend/src/components/UI/Button.tsx
+++ gamefication-frontend/src/components/UI/Button.tsx
@@ -1,12 +1,13 @@
 type Props = {
     text:string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 export const Button: React.FC<Props> = (props) => {
 
     return (
         <>
             <button
-
+                    onClick={props.onClick}
                     className="mt-12 px-8 py-0 rounded-full bg-yellow-400 text-black font-bold">{props.text}
 
             </button>
