b9958c866c66e949d5358f717808242bc02aae8c
MrAziAti <aziz.atif87@gmail.com>
2023-02-26 18:19:53 +0100

    Moved the part that is dependent on the state to render inside a function call.

--- gamefication-frontend/src/components/Pages/GamePage.tsx
+++ gamefication-frontend/src/components/Pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import React, {createRef, useRef, useState} from 'react';
+import React, {useState} from 'react';
 import NewCard from "../UI/NewCard";
 import {Title} from "../Title/Title";
 import Questions from "../Questions/Questions";
@@ -130,6 +130,54 @@ const GamePage = () => {
             console.log(error.message)
         })
     }
+
+    const codeEditor = () => {
+        return (
+            <div className='min-h-screen max-h-screen max-w-screen'>
+                <Header/>
+                <div className='flex flex-col lg:flex-row justify-between items-stretch '>
+                    <div
+                        className='basis-2/6 max-h-[88vh] min-w-[300px] min-h-[400px] whitespace-pre-wrap overflow-x-hidden bg-gameComps resize-x p-4 shadow-2xl m-4 '>
+                        <Problem description={String(task?.description)}
+                                 input={String(task?.testCases[0].input)}
+                                 output={String(task?.testCases[0].output)}/>
+                    </div>
+                    <div className='flex flex-col basis-4/6 max-h-[88vh] m-4'>
+                        <div
+                            className='overflow-auto resize h-screen shadow-2xl bg-gameComps p-4'>
+
+                            <GameEditor onChange={setCode}/>
+
+                        </div>
+
+                        <div className='flex flex-col sm:flex-row '>
+                            <div
+                                className='flex flex-row items-center justify-center basis-4/6 overflow-auto overflow-y-hidden bg-gameComps mt-2 p-4'>
+                                {testCases.map((test, index) => {
+                                    return (
+
+                                        <div className='ml-8 px-4 flex-grow border-t border-gray-400'>
+                                            <ToolTip message={(String)('Testcase:' + index)}>
+                                                <TestCases input={test.input} output={test.output}
+                                                           onClick={() => testCaseHandler(index)}/>
+                                            </ToolTip>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className='justify-between basis-2/6 bg-gameComps mt-2 ml-2'>
+                                <Actions text={buttonText} test='TestAll' handleOnClick={submitHandler}
+                                         handleOnClickTest={testCaseHandler}/>
+                                {taskResultCheck && <RulesModal/>}
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
     return (
         <>
             {state &&
@@ -139,57 +187,15 @@ const GamePage = () => {
                         <NewCard>
                             <div>
                                 <Title title="Velg neste utfordring"/>
-                                <Questions onClick={fetchTask} />
+                                <Questions onClick={fetchTask}/>
                             </div>
                             <ProgressBar/>
                         </NewCard>
                     </div>
                 </>}
             {!state &&
-                <>
-                    <div className='min-h-screen max-h-screen max-w-screen'>
-                        <Header/>
-                        <div className='flex flex-col lg:flex-row justify-between items-stretch '>
-                            <div
-                                className='basis-2/6 max-h-[88vh] min-w-[300px] min-h-[400px] whitespace-pre-wrap overflow-x-hidden bg-gameComps resize-x p-4 shadow-2xl m-4 '>
-                                <Problem description={String(task?.description)}
-                                         input={String(task?.testCases[0].input)}
-                                         output={String(task?.testCases[0].output)}/>
-                            </div>
-                            <div className='flex flex-col basis-4/6 max-h-[88vh] m-4'>
-                                <div
-                                    className='overflow-auto resize h-screen shadow-2xl bg-gameComps p-4'>
-
-                                    <GameEditor onChange={setCode}/>
-
-                                </div>
-
-                                <div className='flex flex-col sm:flex-row '>
-                                    <div
-                                        className='flex flex-row items-center justify-center basis-4/6 overflow-auto overflow-y-hidden bg-gameComps mt-2 p-4'>
-                                        {testCases.map((test, index) => {
-                                            return (
-
-                                                <div className='ml-8 px-4 flex-grow border-t border-gray-400'>
-                                                    <ToolTip message={(String)('Testcase:' + index)}>
-                                                        <TestCases input={test.input} output={test.output}
-                                                                   onClick={() => testCaseHandler(index)}/>
-                                                    </ToolTip>
-                                                </div>
-                                            );
-                                        })}
-                                    </div>
-                                    <div className='justify-between basis-2/6 bg-gameComps mt-2 ml-2'>
-                                        <Actions text={buttonText} test='TestAll' handleOnClick={submitHandler}
-                                                 handleOnClickTest={testCaseHandler}/>
-                                        {taskResultCheck && <RulesModal/>}
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>}
+                codeEditor()
+            }
             <RulesButton openModal={openModal}/>
             <RulesModal visible={modalIsOpen} onClose={closeModal}/>
         </>
