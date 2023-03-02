7e61c83acc8b40601cc4871868274ff3785af1a8
chjsdmn <chjsdmn@hotmail.com>
2023-03-02 13:22:50 +0100

    added animation

--- gamefication-frontend/src/components/UI/NewCard.tsx
+++ gamefication-frontend/src/components/UI/NewCard.tsx
@@ -1,7 +1,7 @@
 const NewCard = (props: any) => {
     return (
         <div
-            className='text-center min-w-fit px-20 my-10 md:mx-52 sm:mx-46 overflow-visible mx-auto rounded-3xl border-4 border-yellow-400 relative py-10'>
+            className='group text-center min-w-fit px-20 my-10 md:mx-52 sm:mx-46 overflow-visible mx-auto rounded-3xl border-4 border-yellow-400 relative py-10 shadow-md shadow-yellow-500'>
             {props.children}
         </div>
     );
