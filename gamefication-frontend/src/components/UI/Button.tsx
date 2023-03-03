052f380485032c45cf93475013dfe2b96993914b
chjsdmn <chjsdmn@hotmail.com>
2023-03-03 11:34:02 +0100

    added animation

--- gamefication-frontend/src/components/UI/Button.tsx
+++ gamefication-frontend/src/components/UI/Button.tsx
@@ -4,7 +4,7 @@ export const Button = (props: any) => {
         <>
             <button
                 onClick={props.handleOnClick}
-                className="px-8 py-0 rounded-full bg-yellow-400 hover:bg-yellow-300 hover:border-yellow-100 hover:scale-110 text-black font-bold text-xs sm:text-xl border-2 border-yellow-500">{props.text}
+                className="px-8 py-0 rounded-full bg-yellow-400 hover:bg-yellow-300 hover:border-yellow-100 hover:scale-110 text-black font-bold text-xs sm:text-xl border-2 border-yellow-500 group-hover:animate-bounce">{props.text}
             </button>
         </>
     )
