3dd1f751734e35d8923ea6edbb02787ee4e645da
MrAziAti <aziz.atif87@gmail.com>
2023-03-03 09:04:10 +0100

    Testcases will now turn green if testcase is successfull

--- gamefication-frontend/src/components/Game/TestCase.tsx
+++ gamefication-frontend/src/components/Game/TestCase.tsx
@@ -1,25 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type Props = {
     onClick: (id: number) => void;
+    successful : boolean
+
 }
 
 const TestCase = (props: any) => {
-    const [ping, setPing] =useState(false)
+    const unsuccessfulStyle = "bg-red-500 hover:bg-red-700 shadow-2xl shadow-red-500 text-white font-bold py-12 px-12 rounded-full transform hover:scale-125 transition ease-in-out delay-150"
+    const successfulStyle = "bg-green-500 hover:bg-green-700 shadow-2xl shadow-green-500 text-white font-bold py-12 px-12 rounded-full transform hover:scale-125 transition ease-in-out delay-150"
+    const [state, setState] = useState(false)
+    const [style, setStyle] = useState(unsuccessfulStyle)
 
-
+    useEffect(() => {
+        if(props.successful)
+            setStyle(successfulStyle)
+        else
+            setStyle(unsuccessfulStyle)
+    }, [props.successful])
     const onCLick = () => {
         props.onClick(props.id)
-        setPing(!ping)
+        setState(true)
+        setStyle("bg-red-500 hover:bg-red-700 shadow-2xl shadow-red-500 text-white font-bold py-12 px-12 rounded-full transform hover:scale-125 transition ease-in-out delay-150 animate-pulse")
     }
-    return (
-        <div className=''>
-            <button
-                className={`bg-red-500 hover:bg-red-700 shadow-2xl shadow-red-500 text-white font-bold py-12 px-12 rounded-full transform hover:scale-125 transition ease-in-out delay-150 ${(ping === false)? '' : 'animate-pulse'}`}
-                onClick={onCLick}>
 
+    return (
+            <button
+                className={style}
+                onClick={onCLick}>
             </button>
-        </div>
     );
 };
 
--- gamefication-frontend/src/components/Pages/GamePage.tsx
+++ gamefication-frontend/src/components/Pages/GamePage.tsx
@@ -7,11 +7,12 @@ import Problem from "../CodeEditor/Problem";
 import ProgressBar from "../ProgressBar";
 import TestCases from "../Game/TestCases";
 import Actions from "../Game/Actions";
-import {GameTask, TaskResult} from "../models";
+import {GameTask, TaskResult, TestCaseResult} from "../models";
 import RulesModal from "../UI/RulesModal";
 import Header from "../Header/Header";
 import ToolTip from "../ToolTip";
 import LanguageSelector from "../Game/LanguageSelector";
+import testCases from "../Game/TestCases";
 
 
 const GamePage = () => {
@@ -26,6 +27,9 @@ const GamePage = () => {
     const [success, setSuccess] = useState(false);
     const [taskResultFail, setTaskResultFail] = useState<TaskResult>()
     const [taskResultSuccess, setTaskResultSuccess] = useState<TaskResult>()
+    const [ping, setPing] = useState(false)
+    const [succesfulTestCases, setSuccessfulTestCases] = useState<boolean[]>([])
+    let data = '';
 
     let taskLenght = 0;
 
@@ -88,8 +92,9 @@ const GamePage = () => {
             return response
         })
             .then(response => response.json()
-                .then(response => {
+                .then((response:GameTask) => {
                     setTask(response)
+                    setSuccessfulTestCases(new Array<boolean>(response.testCases.length));
                     console.log(response)
                     setEditor(false)
                 })).catch((error: Error) => {
@@ -101,8 +106,25 @@ const GamePage = () => {
     if (task?.testCases.length !== undefined) {
         taskLenght = task?.testCases.length
     }
-    const testCaseHandler = (taskId: number) => {
-        fetch(`https://localhost:7067/api/SubmitTestCase?index=${taskId}`, {
+
+    const testCaseHandler = (taskId : number) => {
+        runTestCase(taskId).then(response => {
+            if (!response.ok)
+                throw new Error("no data")
+            return response
+        })
+
+            .then(response => response.json()
+                .then(response => {
+                    console.log(response)
+
+
+                })).catch((error: Error) => {
+            console.log(error.message)
+        })
+    }
+    const runTestCase = async (taskId: number) => {
+        return await fetch(`https://localhost:7067/api/SubmitTestCase?index=${taskId}`, {
             method: "POST",
             credentials: 'include',
             headers: {
@@ -110,18 +132,9 @@ const GamePage = () => {
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             },
             body: JSON.stringify(code)
-        }).then(response => {
-            if (!response.ok)
-                throw new Error("no data")
-            return response
-        })
-            .then(response => response.json()
-                .then(response => {
-                    console.log(response)
-                })).catch((error: Error) => {
-            console.log(error.message)
         })
     }
+
     const languageHandleOnChange = (event: any) => {
         let value = event.target.value
         setLanguage(value)
@@ -146,10 +159,25 @@ const GamePage = () => {
         })
     }
 
-    const testAllHandler = () => {
-        for (let i = 0; i < taskLenght; i++) {
-            testCaseHandler(i);
+    const validateTestCase =  async (taskId : number) : Promise<boolean> => {
+        let success = await runTestCase(taskId)
+        let results = await success.json()
+        return results.success
+    }
 
+    const testAllHandler = async () => {
+        for (let i = 0; i < taskLenght; i++) {
+            let success = await validateTestCase(i)
+            console.log(success)
+            if(!success) {
+                succesfulTestCases[i] = false
+                setSuccessfulTestCases([...succesfulTestCases])
+                break;
+            }
+            else {
+                succesfulTestCases[i] = true
+                setSuccessfulTestCases([...succesfulTestCases])
+            }
         }
     }
 
@@ -188,8 +216,9 @@ const GamePage = () => {
                                             <div className='ml-8 px-4 flex-grow basis-2/4'>
                                                 <ToolTip
                                                     message={"Input: " + task?.testCases[index].input + "\n" + "Output:" + task?.testCases[index].output}>
-                                                    <TestCases input={test.input} output={test.output}
+                                                    <TestCases taskIndex={index} successful={succesfulTestCases} input={test.input} output={test.output}
                                                                onClick={() => testCaseHandler(index)}
+
                                                     />
                                                 </ToolTip>
                                             </div>
