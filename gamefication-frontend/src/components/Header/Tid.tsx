11dea486dd45c4ab3b813cbda3624869aa5c5e2e
JuanMephisto <adrian@grotter.net>
2023-03-07 13:34:40 +0100

    Added websocket to fetch time

--- gamefication-frontend/src/components/Header/Tid.tsx
+++ gamefication-frontend/src/components/Header/Tid.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
-import { State } from "../models";
-import LoadingSpinner from "../UI/LoadingSpinner";
+import React, {useState, useCallback, useEffect} from "react";
+import useWebSocket, {ReadyState} from 'react-use-websocket';
 
 const Tid = () => {
+    const [time, setTime] = useState("00:00");
+    useWebSocket('wss://localhost:7067/ws', {
+        onOpen: () => console.log('opened'),
+        shouldReconnect: (closeEvent) => true,
+        onMessage: (message) => message.data.arrayBuffer().then((buffer:any) => {
+            let data = new Int32Array(buffer);
+            setTime(formatTime(data[0]))
+        }),
+        onError: (e => console.log(e)),
+    });
 
-    const today = new Date();
-    const tid = today.toLocaleTimeString();  
+    const formatTime = (seconds: number) => {
+        let res = "";
+        let m = Math.floor(seconds/60);
+        let s = seconds%60;
+        res += m<10 ? "0"+m : m;
+        res+= ":";
+        res += s<10 ? "0"+s : s;
+        return res;
+    }
 
     return (
         <>
-            <div>Tid: {tid}</div>
-            
+            <div>Tid: {time}</div>
         </>
     );
 }
