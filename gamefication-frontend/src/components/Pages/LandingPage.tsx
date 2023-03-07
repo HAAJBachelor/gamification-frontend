79852353446eba9c9854e3151d5437b5d6bfaaad
chjsdmn <chjsdmn@hotmail.com>
2023-03-07 14:35:20 +0100

    changed UI on landingpage, click logo to landingpa

--- gamefication-frontend/src/components/Pages/LandingPage.tsx
+++ gamefication-frontend/src/components/Pages/LandingPage.tsx
@@ -49,8 +49,8 @@ const LandingPage = () => {
             <div className='pt-38'>
                 <NewCard>
                     <Title title="Velkommen til Gamification"/>
-                    <div className='flex justify-center cursor-pointer'>
-                        <img onClick={startSession} className="max-w-52 max-h-52 m-6 rounded-2xl group-hover:shadow-md group-hover:shadow-yellow-500" src={yellowFolk} alt="two OXX yellow folk"/>
+                    <div className='flex justify-center'>
+                        <img onClick={startSession} className="max-w-52 max-h-52 m-6 rounded-2xl group-hover:shadow-md group-hover:shadow-yellow-500 cursor-pointer" src={yellowFolk} alt="two OXX yellow folk"/>
                     </div>
                     <div className='flex justify-center align-center'>
                        <Button text="Start" handleOnClick={startSession}></Button>
