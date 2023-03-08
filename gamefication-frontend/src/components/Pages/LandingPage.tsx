856c589f1a0c3613a244702add91d4fcd74c9303
chjsdmn <chjsdmn@hotmail.com>
2023-03-08 13:29:45 +0100

    move group keyword from newcard to landingpage

--- gamefication-frontend/src/components/Pages/LandingPage.tsx
+++ gamefication-frontend/src/components/Pages/LandingPage.tsx
@@ -44,26 +44,27 @@ const LandingPage = () => {
                 console.log(error.message)
             })
         }
-            console.log("hei")
         return (
             <div className='pt-38'>
                 <NewCard>
                     <Title title="Velkommen til Gamification"/>
-                    <div className='flex justify-center'>
-                        <img onClick={startSession} className="max-w-52 max-h-52 m-6 rounded-2xl group-hover:shadow-md group-hover:shadow-yellow-500 cursor-pointer" src={yellowFolk} alt="two OXX yellow folk"/>
-                    </div>
-                    <div className='flex justify-center align-center'>
-                       <Button text="Start" handleOnClick={startSession}></Button>
-                        
-                        {/* <svg className="group-hover:animate-bounce h-10 w-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                            <g id="SVGRepo_iconCarrier"> 
-                                <path opacity="0.1" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#FACC14"></path> 
-                                <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#FACC14" stroke-width="2"></path> 
-                                <path d="M12 8L12 16" stroke="#FACC14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
-                                <path d="M15 11L12.087 8.08704V8.08704C12.039 8.03897 11.961 8.03897 11.913 8.08704V8.08704L9 11" stroke="#FACC14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
-                            </g>
-                        </svg>*/}
+                    <div className='group'>
+                        <div className='flex justify-center'>
+                            <img onClick={startSession} className="max-w-52 max-h-52 m-6 rounded-2xl group-hover:shadow-md group-hover:shadow-yellow-500 cursor-pointer" src={yellowFolk} alt="two OXX yellow folk"/>
+                        </div>
+                        <div className='flex justify-center align-center'>
+                        <Button text="Start" handleOnClick={startSession}></Button>
+                            
+                            {/* <svg className="group-hover:animate-bounce h-10 w-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier"> 
+                                    <path opacity="0.1" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#FACC14"></path> 
+                                    <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#FACC14" stroke-width="2"></path> 
+                                    <path d="M12 8L12 16" stroke="#FACC14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+                                    <path d="M15 11L12.087 8.08704V8.08704C12.039 8.03897 11.961 8.03897 11.913 8.08704V8.08704L9 11" stroke="#FACC14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+                                </g>
+                            </svg>*/}
+                        </div>
                     </div>
                 </NewCard>
                 <RulesButton openModal={openModal}/>
