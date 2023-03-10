fdea38bbacf3b77b31131cf50215aeea0dccb452
MrAziAti <aziz.atif87@gmail.com>
2023-03-10 09:38:26 +0100

    Deleted unused component (RulesPage)

--- gamefication-frontend/src/App.tsx
+++ gamefication-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import RulesPage from "./components/Pages/RulesPage";
+
 import GamePage from './components/Pages/GamePage';
 import LandingPage from "./components/Pages/LandingPage";
 
@@ -16,7 +16,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<LandingPage/>}/>
                     <Route path='/game' element={<GamePage/>}/>
-                    <Route path="/rules" element={<RulesPage/>}/>
+
                 </Routes>
             </Router>
 
