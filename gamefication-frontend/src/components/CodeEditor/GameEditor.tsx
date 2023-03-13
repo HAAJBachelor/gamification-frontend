ce8744e0776a25b0b7005774291e34b80589c16a
JorBernt <berntsen.jorgen@gmail.com>
2023-03-13 14:21:38 +0100

    Added syntax support for custom "readline" function for typescript

--- gamefication-frontend/src/components/CodeEditor/GameEditor.tsx
+++ gamefication-frontend/src/components/CodeEditor/GameEditor.tsx
@@ -41,6 +41,12 @@ const GameEditor = (props: Props) => {
         })
     }
 
+    const beforeMount = (monaco: any) => {
+        monaco.languages.typescript.typescriptDefaults.addExtraLib(
+            ['declare function readline(): string;'
+            ].join('\n'));
+    }
+
     return (
         <>
             <Editor
@@ -62,6 +68,7 @@ const GameEditor = (props: Props) => {
                 language={props.lang ? props.lang : 'java'}
                 defaultValue='//'
                 theme={"vs-dark"}
+                beforeMount={beforeMount}
                 onChange={handleEditorChange}
                 value={boilerCode}
             />
