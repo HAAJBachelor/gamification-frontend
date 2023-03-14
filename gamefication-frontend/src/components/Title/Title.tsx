803d7f366a05a099be37cb0938e039cdefacfabf
MrAziAti <aziz.atif87@gmail.com>
2023-03-14 11:14:34 +0100

    added margin to border in  title

--- gamefication-frontend/src/components/Title/Title.tsx
+++ gamefication-frontend/src/components/Title/Title.tsx
@@ -2,8 +2,8 @@
 export const Title = (props: {title: string}) => {
     return (
         <div className={'flex flex-col justify-center items-center'}>
-        <h1 className="sm:text-3xl text-white mb-3 ">{props.title}</h1>
-        <p className={'border-b-4 w-96 text-center border-yellow-500'}></p>
+        <h1 className="sm:text-3xl text-white mb-4 ">{props.title}</h1>
+        <p className={'border-b-8 w-96 text-center border-yellow-500 rounded-xl mb-8'}></p>
     </div>
     )
 }
