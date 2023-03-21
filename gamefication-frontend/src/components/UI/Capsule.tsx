12b4c51b22844834f34350c6ef7df6d2c9ca7855
MrAziAti <aziz.atif87@gmail.com>
2023-03-21 08:51:31 +0100

    removed some warnings

--- gamefication-frontend/src/components/UI/Capsule.tsx
+++ gamefication-frontend/src/components/UI/Capsule.tsx
@@ -7,8 +7,8 @@ type Props = {
     index: number
 }
 const Capsule = (props: Props) => {
-    const delay = props.index == 1 ? "" : props.index === 2 ? "delay-100" : "delay-200";
-    const ani = props.index == 1 ? "animate-[scaleupdownopacity_0.8s_ease-in-out_1]" : props.index == 2 ? "animate-[scaleupdownopacity_1s_ease-in-out_1]" : "animate-[scaleupdownopacity_1.2s_ease-in-out_1]"
+    const delay = props.index === 1 ? "" : props.index === 2 ? "delay-100" : "delay-200";
+    const ani = props.index === 1 ? "animate-[scaleupdownopacity_0.8s_ease-in-out_1]" : props.index === 2 ? "animate-[scaleupdownopacity_1s_ease-in-out_1]" : "animate-[scaleupdownopacity_1.2s_ease-in-out_1]"
     const capsule = ani + " items-center rounded-full font-semibold text-center max-w-none h-8 my-[5px] flex relative group-hover:scale-110 transition-all duration-300 " + delay;
     return (
         <>
